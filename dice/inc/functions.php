8c11651057c65d465a51edef16e4ab838b08b232
Doan Dinh <ddinh@csumb.edu>
2018-11-04 06:03:56 +0000

    Add dice game

---
+++ dice/inc/functions.php
@@ -0,0 +1,58 @@
+<?php session_start();
+
+        function displayDice($humanRoll1, $humanRoll2, $compRoll1, $compRoll2){
+            $dice1 = array ( 1 => "<img src='img/dice1.png'>","<img src='img/dice2.png'>","<img src='img/dice3.png'>","<img src='img/dice4.png'>","<img src='img/dice5.png'>","<img src='img/dice6.png'>"  ); 
+            $dice2 = array ( 1 => "<img src='img/dice1.png'>","<img src='img/dice2.png'>","<img src='img/dice3.png'>","<img src='img/dice4.png'>","<img src='img/dice5.png'>","<img src='img/dice6.png'>"  ); 
+
+            echo "<h3>You rolled a $humanRoll1 and a $humanRoll2</h3>";
+            echo "$dice1[$humanRoll1]";
+            echo "  $dice1[$humanRoll2]";
+            
+            echo "<h3>The computer rolled a $compRoll1 and a $compRoll2</h3>";
+            echo "$dice2[$compRoll1]";
+            echo "  $dice2[$compRoll2]";
+            
+
+        }
+        
+        function calculatePoints($humanSum, $compSum){
+
+            echo "<div id='output'>";
+
+            if($humanSum > $compSum){
+                echo "Congratulations. You have won! Scored $humanSum to $compSum";
+                $_SESSION['humanWin']++;
+            }else if($compSum > $humanSum){
+                echo "Sorry! The computer won. Scored $compSum to $humanSum" ;
+                $_SESSION['computerWin']++;
+    		}else if($compSum == $humanSum){
+                echo "It was a tie! Both are scored $compSum";
+    		}
+            echo "</div>";
+        }
+        
+        function play() {
+            
+            $humanRoll1 = rand(1,6);
+            $humanRoll2 = rand(1,6);
+            
+            $compRoll1 = rand(1,6);
+            $compRoll2 = rand(1,6);
+            
+            $humanSum = $humanRoll1 + $humanRoll2;
+            $compSum = $compRoll1 + $compRoll2;
+
+            calculatePoints($humanSum, $compSum);
+            displayDice($humanRoll1, $humanRoll2, $compRoll1, $compRoll2);
+
+            $_SESSION['human'] = $humanSum;
+            $_SESSION['comp'] = $compSum;
+            /*
+            for ($i=1; $i<7; $i++){
+                ${"randomValue" .$i } = rand(1,6);
+                    displaySymbol(${"randomValue" . $i }, $i);
+                }
+            displayPoints($randomValue1, $randomValue2, $randomValue3);
+            */
+        }
+?>
---
+++ dice/index.php
@@ -0,0 +1,81 @@
+<?php 
+    session_start() ;
+
+    if (isset($_POST["human"])) {
+        if ($_SESSION["array"] != "") {
+            $_SESSION["array"] .= ", " ;
+        }
+
+        $_SESSION["array"] .= $_POST["human"] ;
+    }
+    else {
+        $_SESSION["array"] = "" ;
+        $_SESSION['humanWin'] = 0;
+    }
+    
+    if (isset($_POST["comp"])) {
+        if ($_SESSION["arraycomp"] != "") {
+            $_SESSION["arraycomp"] .= ", " ;
+        }
+
+        $_SESSION["arraycomp"] .= $_POST["comp"] ;
+    }
+    else {
+        $_SESSION["arraycomp"] = "" ;
+        $_SESSION['computerWin'] = 0;
+    }
+    
+    $demo   = $_SESSION["array"] == "" ? "0" : $_SESSION["array"] ;
+    $arrComp   = $_SESSION["arraycomp"] == "" ? "0" : $_SESSION["arraycomp"] ;
+    $humanWin = $_SESSION['humanWin'];
+    $computerWin = $_SESSION['computerWin'];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Dice </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Dice Game</h1>
+        </header>    
+        <div id="main">
+            <?php 
+                include 'inc/functions.php';
+
+                play();
+                echo "<h2>Human Score history: $demo</h2>";
+                echo "<h3>Human win: $humanWin</h3>";
+                echo "<h2>Al Score history: $arrComp</h2>";
+                echo "<h3>Al win: $computerWin</h3>";
+            ?>
+
+            <form action="" method="post">
+                <input id="human" type="hidden" name="human" value= <?php echo $_SESSION['human'] ?> />
+                <input id="comp" type="hidden" name="comp" value= <?php echo $_SESSION['comp'] ?> />
+                <br>
+                <br>
+                <div class="wrapper">
+                    <button class="button">Roll</button>
+                </div>    
+            </form>
+            
+        
+        </div>   
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            CST 336 Internet Programming. 2018&copy; Dinh <br />
+            <strong>Disclaimer;</strong> The information in this webpage is fictious. <br />
+            It is used for academic purpose only.
+            <br />
+            <img src="img/csumb.png" alt="CSUMB logo">            
+        </footer>
+        
+    </body>
+</html>
